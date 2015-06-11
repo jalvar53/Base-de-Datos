d6f5c3eb52b86cc3c26362f9923a0404fa230219
Alfred951 <lutisluis@gmail.com>
2015-06-11 15:18:38 -0500

    Historial Finalizado

--- RDCC/src/Interfaz/Historial.java
+++ RDCC/src/Interfaz/Historial.java
@@ -1,7 +1,15 @@
 package Interfaz;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JTable;
+
 public class Historial extends javax.swing.JFrame {
 
+    Statement stmt = null;
+    ResultSet rs = null;
+        
     public Historial() {
         initComponents();
     }
@@ -10,22 +18,175 @@ public class Historial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        BtBuscar = new javax.swing.JButton();
+        BtSalir = new javax.swing.JButton();
+        jlabel1 = new javax.swing.JLabel();
+        ID = new javax.swing.JTextField();
+        jlabel2 = new javax.swing.JLabel();
+        Placa = new javax.swing.JTextField();
+        jlabel3 = new javax.swing.JLabel();
+        AñoInicio = new javax.swing.JTextField();
+        MesInicio = new javax.swing.JTextField();
+        DiaInicio = new javax.swing.JTextField();
+        jlabel4 = new javax.swing.JLabel();
+        AñoFin = new javax.swing.JTextField();
+        MesFin = new javax.swing.JTextField();
+        DiaFin = new javax.swing.JTextField();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        BtBuscar.setText("Buscar");
+        BtBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtBuscarActionPerformed(evt);
+            }
+        });
+
+        BtSalir.setText("Salir");
+        BtSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BtSalirActionPerformed(evt);
+            }
+        });
+
+        jlabel1.setText("Identificacion:");
+
+        jlabel2.setText("Placa:");
+
+        jlabel3.setText("Fecha Inicio:");
+
+        jlabel4.setText("Fecha Fin:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(BtBuscar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(BtSalir))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jlabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(ID))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jlabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(Placa))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jlabel4)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(AñoFin, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(MesFin, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(DiaFin, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jlabel3)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(AñoInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 60, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addComponent(MesInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(DiaInicio, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 11, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlabel1)
+                    .addComponent(ID, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(Placa, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jlabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlabel3)
+                    .addComponent(AñoInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(MesInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(DiaInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jlabel4)
+                    .addComponent(AñoFin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(MesFin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(DiaFin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(BtBuscar)
+                    .addComponent(BtSalir))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void BtSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtSalirActionPerformed
+        OpcionesAdmin opc = new OpcionesAdmin();
+        opc.setVisible(true);
+        opc.setLocationRelativeTo(null);
+        this.dispose();
+    }//GEN-LAST:event_BtSalirActionPerformed
+
+    private void BtBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtBuscarActionPerformed
+        try{
+            stmt = Conexion.link.createStatement();
+            rs = stmt.executeQuery(getQuery());
+            JTable table = new JTable(Conexion.buildTableModel(rs));
+            Resultados resultado;
+            resultado = new Resultados(table);
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }  
+    }//GEN-LAST:event_BtBuscarActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextField AñoFin;
+    private javax.swing.JTextField AñoInicio;
+    private javax.swing.JButton BtBuscar;
+    private javax.swing.JButton BtSalir;
+    private javax.swing.JTextField DiaFin;
+    private javax.swing.JTextField DiaInicio;
+    private javax.swing.JTextField ID;
+    private javax.swing.JTextField MesFin;
+    private javax.swing.JTextField MesInicio;
+    private javax.swing.JTextField Placa;
+    private javax.swing.JLabel jlabel1;
+    private javax.swing.JLabel jlabel2;
+    private javax.swing.JLabel jlabel3;
+    private javax.swing.JLabel jlabel4;
     // End of variables declaration//GEN-END:variables
+    public String getQuery(){
+        String query = "SELECT * FROM Renta WHERE ";
+        if(!ID.getText().isEmpty()){
+            query = query + "`Identificacion` = " + "'" + ID.getText() + "'" + " AND ";
+        }
+        if(!Placa.getText().isEmpty()){
+            query = query + "`Placa` = " + "'" + Placa.getText() + "'" + " AND ";
+        }
+        if(!AñoInicio.getText().isEmpty() && !MesInicio.getText().isEmpty() && !DiaInicio.getText().isEmpty()){
+            query = query + "`Fecha Ini` = " + "'" + AñoInicio.getText() + "-" + MesInicio.getText() + "-" + DiaInicio.getText() + "'" + " AND ";
+        }
+        if(!AñoFin.getText().isEmpty() && !MesFin.getText().isEmpty() && !DiaFin.getText().isEmpty()){
+            query = query + "`Fecha Fin` = " + "'" + AñoFin.getText() + "-" + MesFin.getText() + "-" + DiaFin.getText() + "'" + " AND ";
+        }
+        if(!ID.getText().isEmpty() || !Placa.getText().isEmpty() || !AñoInicio.getText().isEmpty() || !AñoFin.getText().isEmpty()){
+            query = query.substring(0, query.length()-4);
+        }
+        else{
+            query = query.substring(0, query.length()-5);
+        }
+        return query;
+    }    
 }
